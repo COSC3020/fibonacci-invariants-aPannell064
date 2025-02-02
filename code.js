d41292d8a101e41c998c919b6dad2ac16c9f6a3c
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-02-01 19:05:20 -0700

    Clean up code.js

--- code.js
+++ code.js
@@ -1,20 +1,9 @@
 function fib(n) 
 {
-    var fibs = [];
-    if(n == 0) {
-        fibs[n] = n; 
-    }
-
-    else if (n > 0) {
-        fibs = fib(n-1);
-
-        if (n == 1) {
-            fibs[n] = n
-        }
-
-        else {
-        fibs[n] = fibs[n-1] + fibs[n-2];
-        }
-    }
+    if(n == 0) {return [0]}
+    else if (n == 1) {return [0, 1]}
+    
+    var fibs = fib(n-1);
+    fibs[n] = fibs[n-1] + fibs[n-2];
     return fibs;
 }
