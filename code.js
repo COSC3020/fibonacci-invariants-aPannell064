6c3d2cb7c9645c2142cebcb2ea8d4b7edfa0c24c
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-02-03 08:34:12 -0700

    Ignore invalid input

--- code.js
+++ code.js
@@ -2,8 +2,10 @@ function fib(n)
 {
     if(n == 0) {return [0]}
     else if (n == 1) {return [0, 1]}
-    
-    var fibs = fib(n-1);
-    fibs[n] = fibs[n-1] + fibs[n-2];
-    return fibs;
+    else if (n > 1) { 
+        var fibs = fib(n-1);
+        fibs[n] = fibs[n-1] + fibs[n-2];
+        return fibs;
+    }
+    return [];
 }
