8f7e7ccd45c6eaad82693fff57f66d43bcf322cf
aPannell064 <157420768+aPannell064@users.noreply.github.com>
2025-03-02 03:16:15 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,16 @@
 function fib(n) 
-{
+{    
+    //Base Cases
     if(n == 0) {return [0]}
     else if (n == 1) {return [0, 1]}
+
+    //Recursive Case
     else if (n > 1) { 
         var fibs = fib(n-1);
         fibs.push(fibs[n-1] + fibs[n-2]);
         return fibs;
     }
+
+    //Excluded Case
     return [];
 }
